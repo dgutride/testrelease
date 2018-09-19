7655e9e8d3dc4eba829416c9d61860c59ebf3c49
Dana Gutride <dgutride@redhat.com>
2018-09-19 10:46:08 -0400

    fix(releases): add semantic release custom script

---
+++ build/testtest.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+default()
+{
+  TRAVIS_BUILD_DIR=.
+  BUILD_DIR=$TRAVIS_BUILD_DIR
+  BOWER_JSON=$BUILD_DIR/bower.json
+  cd $BUILD_DIR
+  BOWER_VERSION='3.5.3'
+  VERSION='3.5.4'
+  sed "s|\"version\":.*|\"version\": \"$VERSION\",|" $BOWER_JSON > $BOWER_JSON.tmp
+
+  check $? "Version bump failure"
+
+  if [ -s "$BOWER_JSON.tmp" ]; then
+    mv $BOWER_JSON.tmp $BOWER_JSON
+    check $? "File move failure"
+  fi
+}
+
+# Check errors
+#
+# $1: Exit status
+# $2: Error message
+# $3: Show warning
+check()
+{
+  if [ "$1" != 0 ]; then
+    if [ "$3" = "warn" ]; then
+      printf "\n"
+      echo "*** Warning: $2"
+    else
+      printf "\n"
+      echo "*** Error: $2"
+      exit $1
+    fi
+  fi
+}
+
+# main()
+{
+  default
+}
